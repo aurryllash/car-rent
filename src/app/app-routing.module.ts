a4a1db8389a84cc9cb476b750020923d08ee7307
aurryllash <lashatsiklauri77@gmail.com>
2024-02-25 16:51:28 +0400

    make testimonials component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { AboutComponent } from './Components/about/about.component';
 import { ModelsComponent } from './Components/models/models.component';
+import { TestimonialsComponent } from './Components/testimonials/testimonials.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'models', component: ModelsComponent}
+  {path: 'models', component: ModelsComponent},
+  {path: 'testimonials', component: TestimonialsComponent}
 ];
 
 @NgModule({
