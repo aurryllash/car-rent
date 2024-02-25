bee1d59116e4366c3413329b582ab4b418b74154
aurryllash <lashatsiklauri77@gmail.com>
2024-02-25 15:29:09 +0400

    make models component

--- src/app/Components/models/models.component.ts
+++ src/app/Components/models/models.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MainService } from '../../Services/main.service';
+import { Car } from '../../Interfaces/product';
 
 @Component({
   selector: 'app-models',
   templateUrl: './models.component.html',
   styleUrl: './models.component.css'
 })
-export class ModelsComponent {
+export class ModelsComponent implements OnInit{
+  cars: Car[] = []
+  constructor(private main: MainService) {}
+  ngOnInit(): void {
+    this.main.getAllCar().subscribe(response => {
+      this.cars = response
+    })
+  }
+
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { AboutComponent } from './Components/about/about.component';
+import { ModelsComponent } from './Components/models/models.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'models', component: ModelsComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
