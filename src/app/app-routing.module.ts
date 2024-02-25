137b1c64f541503b2ecdf2aa8d65768ceb1cf236
aurryllash <lashatsiklauri77@gmail.com>
2024-02-25 17:31:43 +0400

    make team component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { HomeComponent } from './Components/home/home.component';
 import { AboutComponent } from './Components/about/about.component';
 import { ModelsComponent } from './Components/models/models.component';
 import { TestimonialsComponent } from './Components/testimonials/testimonials.component';
+import { TeamComponent } from './Components/team/team.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'models', component: ModelsComponent},
-  {path: 'testimonials', component: TestimonialsComponent}
+  {path: 'testimonials', component: TestimonialsComponent},
+  {path: 'team', component: TeamComponent},
+  {path: 'contact', component: ContactComponent}
+  
 ];
 
 @NgModule({
